b0aee5529d1dc6d4d166655a02843268b51d937a
RomanNet1 <ramulrimov004@gmail.com>
2024-02-01 18:00:39 +0300

    2_11

--- src/school/L2/L_2_11/Main.java
+++ src/school/L2/L_2_11/Main.java
@@ -1,5 +1,11 @@
 package school.L2.L_2_11;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("**********");
+        System.out.println("* QWERTY *");
+        System.out.println("* 123456 *");
+        System.out.println("**********");
+    }
 
 }
