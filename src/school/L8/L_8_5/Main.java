ef7309d30764204a508f0e4474f996d304160382
RomanNet1 <ramulrimov004@gmail.com>
2024-02-05 02:35:41 +0300

    8_5

---
+++ src/school/L8/L_8_5/Main.java
@@ -0,0 +1,56 @@
+package school.L8.L_8_5;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 5, a = 4;
+        if (x>0){
+            System.out.println("+");
+        }else {
+            System.out.println("no +");
+        }
+        if (x>a){
+            a = -1;
+        }else {
+            a = 0;
+        }
+        if (a>100){
+            System.out.println("Ok");
+        }else {
+            System.out.println("error");
+        }
+        if (a!=x){
+            a = x;
+        }else {
+            x = 2*a;
+        }
+        if (x>0){
+            x++;
+        }else {
+            x--;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
