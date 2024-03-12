22b4dd2bdd735add7d6bc2862fc9937326365362
RomanNet1 <ramulrimov004@gmail.com>
2024-03-12 11:13:04 +0300

    2.13

--- src/school/L2/L_2_13/Main.java
+++ src/school/L2/L_2_13/Main.java
@@ -1,4 +1,13 @@
 package school.L2.L_2_13;
 
 public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+
+        int c = a + b;
+        System.out.println(c);
+        System.out.println("a + b = " + c);
+
+    }
 }
