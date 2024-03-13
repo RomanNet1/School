cc3e2c3c3d843df673685514fa26c755e16ae11c
RomanNet1 <ramulrimov004@gmail.com>
2024-03-14 02:12:01 +0300

    z1 3

--- src/uchebnikTeoria/zadania/z1/Main.java
+++ src/uchebnikTeoria/zadania/z1/Main.java
@@ -2,10 +2,27 @@ package uchebnikTeoria.zadania.z1;
 
 import uchebnikTeoria.data_p2.Date;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Date[] arrDate = new Date[10];
         int a = arrDate.length;
-        MassivDate.arrDateZ(arrDate, a);
+        Date[] dd =  MassivDate.arrDateZ(arrDate, a);
+        //1
+        System.out.println(Arrays.toString(dd));
+        //2
+        MassivDate.checkDataDayError(dd);
+        //3
+        MassivDate.refDataDayError(dd);
+
+
+
+
+
+
+
+
+
     }
 }
--- src/uchebnikTeoria/zadania/z1/MassivDate.java
+++ src/uchebnikTeoria/zadania/z1/MassivDate.java
@@ -5,12 +5,14 @@ import uchebnikTeoria.data_p2.Date;
 import java.util.Arrays;
 
 public class MassivDate {
-    public static void arrDateZ(Date[] arrDate, int a) {
+    public static Date[] arrDateZ(Date[] arrDate, int a) {
 
         for (int i = 0; i < a; i++) {
             arrDate[i] = new Date(random(1, 31), random(1, 12), random(1900, 2011));
         }
-        System.out.println(Arrays.toString(arrDate));
+        //System.out.println(Arrays.toString(arrDate));
+        Date [] d = arrDate;
+        return d;
     }
 
 
@@ -19,4 +21,31 @@ public class MassivDate {
     }
 
 
+    public static void checkDataDayError(Date[] dd) {
+        int schet = 0;
+        for (int i = 0; i < dd.length; i++) {
+            if(dd[i].getDay() > 31 || dd[i].getDay() < 1 ){
+                schet++;
+            }
+        }
+        System.out.println("Число ошибочных дат - " + schet);
+    }
+
+    public static void refDataDayError(Date[] dd) {
+        int schet = 0;
+        for (int i = 0; i < dd.length; i++) {
+            if(dd[i].getDay() > 31 ){
+                dd[i].setDay(31);
+                schet++;
+            } else if (dd[i].getDay() < 1) {
+                dd[i].setDay(1);
+                schet++;
+            }else {
+                //
+                 }
+
+        }
+        System.out.println("Число ошибочных дат исправлено - " + schet);
+        System.out.println(Arrays.toString(dd));
+    }
 }
