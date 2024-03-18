269f887a8ca4c267c470c605c93d8cb5efa9b175
RomanNet1 <ramulrimov004@gmail.com>
2024-03-18 03:12:05 +0300

    test

---
+++ src/school/L17/Test.java
@@ -0,0 +1,21 @@
+package school.L17;
+
+public class Test {
+}
+
+//public class App {
+//
+//    public static void main(String[] args) {
+//        System.out.println(sumOfDigits(123)); // => 6
+//    }
+//
+//    public static int sumOfDigits(int number) {
+//        number = Math.abs(number);
+//        int sum = 0;
+//        while (number > 0) {
+//            sum += number % 10;
+//            number /= 10;
+//        }
+//        return sum;
+//    }
+//}
